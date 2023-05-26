554617d48e2ef6d2ce719a0d29ea0dba2cc1e984
Erfan Abdi <erfangplus@gmail.com>
2023-05-27 00:13:02 +0330

    GS5: adapt to LMODroid using script

--- mimameid/lmodroid_mimameid.mk
+++ mimameid/lmodroid_mimameid.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 
 # Inherit from mimameid device
 $(call inherit-product, device/gigaset/GS5-unified/device.mk)
@@ -27,7 +27,7 @@ $(call inherit-product, device/gigaset/GS5-unified/device.mk)
 PRODUCT_BRAND := volla
 PRODUCT_DEVICE := mimameid
 PRODUCT_MANUFACTURER := Volla
-PRODUCT_NAME := lineage_mimameid
+PRODUCT_NAME := lmodroid_mimameid
 PRODUCT_MODEL := Volla Phone 22
 
 # Build info
