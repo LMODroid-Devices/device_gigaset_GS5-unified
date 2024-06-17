05fab8e94d076b804cc30273e5f7408a4bfc8f65
Erfan Abdi <erfangplus@gmail.com>
2024-06-17 19:23:32 +0330

    GS5: Customize vendor androidmk for firmwares
    Change-Id: Ie865367bace6219474e9f638398bea2cc9bfcd13

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -34,5 +34,17 @@ write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
 append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
 
+sed -i "/TARGET_DEVICE/d" "$ANDROIDMK"
+sed -i "/add-radio-file/d" "$ANDROIDMK"
+
+cat << EOF >> "$ANDROIDMK"
+ifneq (\$(filter GS5 mimameid,\$(TARGET_DEVICE)),)
+
+RADIO_FILES := \$(wildcard \$(LOCAL_PATH)/radio/\$(TARGET_DEVICE)/*)
+\$(foreach f, \$(notdir \$(RADIO_FILES)), \\
+    \$(call add-radio-file,radio/\$(f)))
+
+EOF
+
 # Finish
 write_footers
